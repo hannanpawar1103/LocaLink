404421f272182cc05ba0d46eab8487f61b8bcd15
Pawar Abdul Hannan <hannanpawar1103@gmail.com>
2024-10-09 20:06:02 +0530

    "user registering"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import {Outlet} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
-   <>
-   <div>App</div>
-   </>
+  <>
+  <Header/>
+  <Outlet/>
+  <Footer/>
+  </>
   )
 }
 
